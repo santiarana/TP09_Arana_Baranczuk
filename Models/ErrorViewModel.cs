674c2deaed2f12e85c493390156bb5ed19ce5736
SANTINO ARANA <47031761@ort>
2022-10-06 09:39:55 -0300

    mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP09_Arana_Baranczuk.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
