fedde2ceae56858a56d887cab9df873038974b11
THIAGO BARANCZUK <46876761@ort>
2022-11-01 09:03:24 -0300

    falta clave

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -14,29 +14,3 @@ function MostrarProducto(IdP, nombre) {
         }
     });
 }
-
-function Password(IdU, clave) {
-    $.ajax({
-        type: 'POST',
-        dataType: 'JSON',
-        url: '/Home/AgregarProducto',
-        data: { IdUsuario: IdU },
-        success: function(response) {
-            $(document).ready(function() {
-                $("#TextoModal").html("Clave:  <input type=number id=CantidadDisponible name=CantidadDisponible required></br>")
-                $("#botonenviar").click(function() { // Con esto establecemos la acción por defecto de nuestro botón de enviar.
-                    if (validaForm()) { // Primero validará el formulario.
-                        $.post("enviar.php", $("#formdata").serialize(), function(res) {
-                            $("#formulario").fadeOut("slow"); // Hacemos desaparecer el div "formulario" con un efecto fadeOut lento.
-                            if (res == 1) {
-                                $("#exito").delay(500).fadeIn("slow"); // Si hemos tenido éxito, hacemos aparecer el div "exito" con un efecto fadeIn lento tras un delay de 0,5 segundos.
-                            } else {
-                                $("#fracaso").delay(500).fadeIn("slow"); // Si no, lo mismo, pero haremos aparecer el div "fracaso"
-                            }
-                        });
-                    }
-                });
-            });
-        }
-    });
-}
